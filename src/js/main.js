9f663c50f8a0e9de896e936ddd9a80d18647019d
hdevhub <nothingformetohappi@gmai.com>
2026-01-20 18:04:33 +0700

    feat: add task logic and fix sctipt path

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-console.log("Task Tracker loaded successfully");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const taskList = document.querySelector(".task-list");
+
+button.addEventListener("click", function () {
+  const taskText = input.value.trim();
+
+  if (taskText === "") {
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  taskList.appendChild(li);
+  input.value = "";
+});
+
